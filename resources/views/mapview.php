b5f8a75940a48a43b54055178dbf171ae3997905
Nick Williams <nick22891@yahoo.com>
2015-11-16 14:43:36 -0500

    .

--- resources/views/mapview.php
+++ resources/views/mapview.php
@@ -1042,6 +1042,13 @@
         #sidebar {
 
             width:60%;
+            display:block;
+
+        }
+
+        .leaflet-sidebar {
+
+
 
         }
 
@@ -1660,7 +1667,7 @@
 
 
 
-    var map = L.map('map').setView([14.06, -67.09], 5);
+    var map = L.map('map').setView([14.06, -71.09], 5);
     //https://a.tiles.mapbox.com/v4/nickjwill.lcnch31p/page.html?access_token=pk.eyJ1Ijoibmlja2p3aWxsIiwiYSI6Im4xQWFQeTQifQ.bwI5KQmy7z7kS9woXzbplw#6/31.625/40.463
     L.tileLayer('http://{s}.tiles.mapbox.com/v4/nickjwill.lcnc6kpo/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoibmlja2p3aWxsIiwiYSI6Im4xQWFQeTQifQ.bwI5KQmy7z7kS9woXzbplw', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
